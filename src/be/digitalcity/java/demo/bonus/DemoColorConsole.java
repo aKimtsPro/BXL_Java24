53e3bf8f65671aa619106dae749eac249b32a4ac
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2024-07-19 15:27:45 +0200

    demo bonus color

---
+++ src/be/digitalcity/java/demo/bonus/DemoColorConsole.java
@@ -0,0 +1,20 @@
+package be.digitalcity.java.demo.bonus;
+
+public class DemoColorConsole {
+
+    public static void main(String[] args) {
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_YELLOW = "\u001B[33m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_CYAN = "\u001B[36m";
+        final String ANSI_WHITE = "\u001B[37m";
+
+        System.out.println(ANSI_RED + "salut");
+        System.out.println(ANSI_RESET + "salut");
+        System.out.println(ANSI_GREEN + "salut");
+    }
+}
